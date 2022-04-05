405ac4dfca382824266830ee97cbccfcd1fa9ffb
Niladri1996 <iamniladri47@gmail.com>
2022-04-05 13:52:28 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,23 @@ class HomePage extends StatelessWidget{
        title: Text("My First Flutter App"),
      ) ,
      body: Container(
-       child: Center(child: Text("Hello World")),
+       child: Center(
+         child: Container(
+           padding: const EdgeInsets.all(12),
+           alignment: Alignment.center,
+           width: 150,
+           height: 150,
+           child: Text("My Name Is Niladri",style: TextStyle(color: Colors.white,fontSize: 14,fontWeight: FontWeight.bold)),
+           decoration: BoxDecoration(
+             color: Colors.lightGreen,
+             gradient: LinearGradient(colors: [Colors.red,
+             Colors.amber]),
+             // shape:BoxShape.circle
+             borderRadius: BorderRadius.circular(10),
+             boxShadow: [BoxShadow(color: Colors.grey,blurRadius: 11,offset: Offset(2.0,5.0))]
+           ),
+         ),
+       ),
      ),
    );
   }
